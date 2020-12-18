c5ba3e437499f9911b7c5c6607704c71648c8284
Diego Montania <diegoasmad1@gmail.com>
2020-12-18 20:43:13 -0300

    Iniciando a implementação de login e senha

---
+++ UniversidadeDiego/Controllers/ContaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversidadeDiego.Controllers
+{
+    public class ContaController : Controller
+    {
+        //retorna view Registrar
+        [HttpGet]
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        //retorna view Login
+        [HttpGet]
+        public async Task<ActionResult> Login()
+        {
+            return View();
+        }
+    }
+}
